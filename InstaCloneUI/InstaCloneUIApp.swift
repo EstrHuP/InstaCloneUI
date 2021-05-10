2da23c9568a3fbf1fd8b68cd1d559cc40d43a34d
Esther Huecas <esthrhup@gmail.com>
2021-05-10 11:32:40 +0200

    Initial Commit

---
+++ InstaCloneUI/InstaCloneUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneUIApp.swift
+//  InstaCloneUI
+//
+//  Created by Esther Huecas on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
